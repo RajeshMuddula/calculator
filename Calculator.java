40c2562d5354a1fbcfbd7530455ab6d98c89489b
RajeshMuddula <muddularajesh@gmail.com>
2025-04-16 21:22:45 +0530

    Create Calculator.java
    Added basic calculator structure

---
+++ Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Calculator\n");
+        System.out.println("Choose one operation:\n1. Add\n2. Subtract\n3. Exit");
+    }
+}
